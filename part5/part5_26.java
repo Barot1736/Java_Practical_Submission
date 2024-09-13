795ab0163753fcb78cef4f1dd312de61f2f5306c
Barot1736 <barotparth1736@gmail.com>
2024-09-13 13:16:51 +0530

    Part 5

---
+++ part5/part5_26.java
@@ -0,0 +1,15 @@
+public class part5_26 {
+    public static void main(String[] args) 
+    throws  Exception{
+        int a=5;
+        int b = 0;
+        if (b==0) {
+            throw new Exception("Divide by zero");
+        }
+        else{
+             System.out.println(a/b);
+
+         }
+        System.out.println("hello");
+    }    
+}
